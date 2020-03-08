08104d65f0487716bc7c1802999c051f2f158481
Franco Tagliero <ftagliero@gmail.com>
2020-03-08 20:58:25 -0300

    Updates

---
+++ precache-manifest.11fb2bef08ceaafc4c29395af89447b2.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2ea249d52b95a80aee384257e67692bc",
+    "url": "/react-monster/index.html"
+  },
+  {
+    "revision": "366bb2edab7077b87b4e",
+    "url": "/react-monster/static/css/main.b1c655a7.chunk.css"
+  },
+  {
+    "revision": "f6cbc1f38bb15a18c333",
+    "url": "/react-monster/static/js/2.fa020b78.chunk.js"
+  },
+  {
+    "revision": "5356fa2f66e46e6c05e4cbe319ac7f1d",
+    "url": "/react-monster/static/js/2.fa020b78.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "366bb2edab7077b87b4e",
+    "url": "/react-monster/static/js/main.49482e19.chunk.js"
+  },
+  {
+    "revision": "b7ae6742dfd536792a55",
+    "url": "/react-monster/static/js/runtime-main.68a8d598.js"
+  }
+]);
